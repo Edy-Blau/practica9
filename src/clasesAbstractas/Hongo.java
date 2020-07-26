162567dd4a230a4bd84f12af9c91080f765ce880
Edy Blau <edwral@hotmail.com>
2020-07-26 16:31:38 -0500

    #5. Se creo la clase Hongo, clase hija de SerVivo.

---
+++ src/clasesAbstractas/Hongo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clasesAbstractas;
+
+/**
+ *
+ * @author edyblau
+ */
+public class Hongo extends SerVivo{
+
+    @Override
+    public void alimentarse() {
+        System.out.println("Nos alimentamos como parásitos o con materia de desecho orgánica");
+    }
+
+    @Override
+    public void respirar() {
+        System.out.println("Respiramos de manera anaerobia, pero también aerobia.");
+    }
+    
+}
